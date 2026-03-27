6957d00b1db3e27694986d304c26fb4aa332dd76
a-anuj <anujamirthalingam5@gmail.com>
2026-03-27 18:06:35 +0530

    chore : practice bool and unsigned int

--- 02variables/main.go
+++ 02variables/main.go
@@ -6,4 +6,12 @@ func main() {
 	var username string = "Anuj"
 	fmt.Println(username)
 	fmt.Printf("Variable is of type : %T\n", username)
+
+	var isLoggedin bool = true
+	fmt.Println(isLoggedin)
+	fmt.Printf("Variable is of the type : %T\n", isLoggedin)
+
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is of the type : %T\n", smallVal)
 }
