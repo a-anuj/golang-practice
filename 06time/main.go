b9a2ba87ba02a4cc374c8d459dbf91623b4f597d
a-anuj <anujamirthalingam5@gmail.com>
2026-04-09 15:12:51 +0530

    chore : basic .Now() function

---
+++ 06time/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Study on time package by go...")
+	presentTime := time.Now()
+	fmt.Println("Current time :", presentTime)
+}
