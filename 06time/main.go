445c94d9f8c02f383466012febeedfd6c3622471
a-anuj <anujamirthalingam5@gmail.com>
2026-04-09 15:23:28 +0530

    feat : create a date

--- 06time/main.go
+++ 06time/main.go
@@ -11,4 +11,7 @@ func main() {
 	fmt.Println("Current time :", presentTime)
 	fmt.Println("After formatting :\n")
 	fmt.Println(presentTime.Format("02-01-2006 15:04:05 Monday"))
+
+	dateCreation := time.Date(2027, time.April, 29, 12, 0, 0, 0, time.UTC)
+	fmt.Println("\n\nCreated Date is :", dateCreation.Format("02-01-2006 Monday"))
 }
