fe621e06406d43be3ea4a5403c0ebe6d746e4773
a-anuj <anujamirthalingam5@gmail.com>
2026-04-10 14:27:29 +0530

    feat : add hello handler

--- simple-go-server/main.go
+++ simple-go-server/main.go
@@ -5,6 +5,17 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Page Not Found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+	}
+	fmt.Fprintf(w, "Hello")
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("static"))
 	http.Handle("/", fileServer)
