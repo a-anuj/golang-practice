e677ff1dd798201e114e3e13bf6b3e246b5ed9be
a-anuj <anujamirthalingam5@gmail.com>
2026-04-10 14:21:37 +0530

    feat : create the main function

--- simple-go-server/main.go
+++ simple-go-server/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
+
+	fmt.Println("Server is starting at port 8080")
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
