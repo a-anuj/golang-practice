2abd181aeedb391de75e307bae946ebe4f1e6de5
a-anuj <anujamirthalingam5@gmail.com>
2026-04-10 12:31:19 +0530

    practice reading files

--- files/main.go
+++ files/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
@@ -16,10 +17,17 @@ func main() {
 	fmt.Println("Gonna write 'Hi' to the file....")
 	file, err := os.Create("test.txt")
 	handleError(err)
-	content := "HEllo"
+	content := "Hii"
 	length, err := io.WriteString(file, content)
 	handleError(err)
 
 	fmt.Println("Total Length written is :", length)
 	defer file.Close()
+	readFile("test.txt")
+}
+
+func readFile(filename string) {
+	data, err := ioutil.ReadFile(filename)
+	handleError(err)
+	fmt.Println("The data present inside the file is :", string(data))
 }
